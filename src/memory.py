5f8fc3912ee68a13819ab50d1b5e0c6c41c0627e
owasikohu <owatas2022@gmail.com>
2025-04-26 23:59:58 +0900

    updte memory.py

--- src/memory.py
+++ src/memory.py
@@ -1,2 +1,16 @@
 import bios
-print(bios.wozmon)
+import config
+
+class memory:
+    def __init__(self):
+        self.ram = [0x00] * config.ramsize
+    
+    def read_memory(address):
+        if address  >= 0xff00:
+            begin_wozmon = 0xff00
+            offseted_address = address - begin_wozmon
+            return bios.wozmon[offseted_address]
+        
+        
+    def write_memory(address, data):
+        return 0
