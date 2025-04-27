92bd5ee45b71d3d687884302fb8b13fd669e4a5e
owasikohu <owatas2022@gmail.com>
2025-04-27 21:36:02 +0900

    update memory.py

--- src/main.py
+++
@@ -1,6 +0,0 @@
-import tkinter
-from cpu import MOS6502 
-
-cpu = MOS6502()
-cpu.re
-print(cpu.reg_a)
--- src/memory.py
+++ src/memory.py
@@ -1,16 +1,22 @@
-import bios
+import rom
 import config
 
 class memory:
     def __init__(self):
         self.ram = [0x00] * config.ramsize
-    
-    def read_memory(address):
-        if address  >= 0xff00:
+
+    def read(address):
+        # read ram
+        if address >= 0x0000 and address <= config.ramsize:
+            begin_ram = 0x0000
+            offseted_address = address - begin_ram
+            return memory.ram[offseted_address]
+        # read wozmon
+        if address  >= 0xff00 and address <= 0xffff:
             begin_wozmon = 0xff00
             offseted_address = address - begin_wozmon
-            return bios.wozmon[offseted_address]
+            return rom.wozmon[offseted_address]
         
         
-    def write_memory(address, data):
+    def write(address, data):
         return 0
